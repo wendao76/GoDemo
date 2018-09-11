f8e67e370bcf9ccba88b8bbf1a8331a687a31843
李文虎 <liwenhu@cyou-inc.com>
2018-09-11 15:09:14 +0800

    测试学习代码提交

---
+++ src/tests/lesson1/TestSlice.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	var arr [20]int
+	arr = [20]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	var arr1 = arr[:]
+	var slice1 = arr1[:]
+	slice12 := append(slice1, 21)
+	fmt.Println(slice12)
+	fmt.Println(cap(slice12))
+
+	var mapData map[int]string = make(map[int]string)
+	mapData[1] = "xiaoming"
+	mapData[2] = "xiaoli"
+	mapData[3] = "xiaowang"
+	for value := range mapData {
+		fmt.Println(mapData[value])
+	}
+
+	var book Book
+	book.title = "三只小猪"
+	book.prize = 25
+	fmt.Println(book)
+
+}
+
+type Book struct {
+	title string
+	prize int
+}

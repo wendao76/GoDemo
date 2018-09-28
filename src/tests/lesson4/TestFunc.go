a7799629798ce2854170ee88052a754d01279f67
李文虎 <liwenhu@cyou-inc.com>
2018-09-28 14:49:59 +0800

    函数测试样例2

--- src/tests/lesson4/TestFunc.go
+++ src/tests/lesson4/TestFunc.go
@@ -6,6 +6,14 @@ func main() {
 	var add = func(a int, b int) int {
 		return a + b
 	}
+	var add2 = createAddFunc()
 
 	fmt.Println(add(5, 10))
+	fmt.Println(add2(5, 10))
+}
+
+func createAddFunc() func(a int, b int) int {
+	return func(a int, b int) int {
+		return a + b
+	}
 }

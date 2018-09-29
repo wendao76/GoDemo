920b3234203a100bb6895dafd7160e2f324694ac
李文虎 <liwenhu@cyou-inc.com>
2018-09-29 14:24:48 +0800

    自定义错误样例

--- src/tests/lesson3/TestError.go
+++ src/tests/lesson3/TestError.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+)
 
+func main() {
+	error := NewStringError("error happned", 500)
+	error.printError()
+}
+
+type StringError struct {
+	msg  string
+	code int
+}
+
+func NewStringError(msg string, code int) *StringError {
+	return &StringError{msg, code}
+}
+
+func (stringError *StringError) printError() {
+	fmt.Println(strconv.Itoa(stringError.code) + " " + stringError.msg)
 }

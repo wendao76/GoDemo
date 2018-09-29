ff5608d7f4e4525b6e07411ee7140c196c3d903e
李文虎 <liwenhu@cyou-inc.com>
2018-09-29 10:20:50 +0800

    函数测试样例3

--- src/tests/lesson4/TestFunc.go
+++ src/tests/lesson4/TestFunc.go
@@ -10,10 +10,25 @@ func main() {
 
 	fmt.Println(add(5, 10))
 	fmt.Println(add2(5, 10))
+	fmt.Println(testMultiParam(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
 }
 
 func createAddFunc() func(a int, b int) int {
 	return func(a int, b int) int {
 		return a + b
 	}
+}
+
+//测试函数参数
+func testFuncParam(a int, funca func(a int, b int)) int {
+	return 1
+}
+
+//测试不定参数
+func testMultiParam(a ...int) int {
+	var returnVal int
+	for _, v := range a {
+		returnVal += v
+	}
+	return returnVal
 }

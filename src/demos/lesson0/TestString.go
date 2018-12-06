f117003ff4dc356c22d21f029d2be3b34bb86729
李文虎 <liwenhu@cyou-inc.com>
2018-12-06 15:18:32 +0800

    1. 字符串函数使用 2. 结构体使用样例添加

---
+++ src/demos/lesson0/TestString.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var str string = "hello_world"
+	fmt.Println(strings.Title(str))
+	fmt.Println(strings.ToUpper(str))
+}
--- src/demos/lesson2/TestStruct.go
+++ src/demos/lesson2/TestStruct.go
@@ -5,12 +5,27 @@ import (
 	"strconv"
 )
 
+//结构体样例
 func main() {
 	var book Book
 	book.title = "我自己的书"
 	book.prize = 25
 	book.printInfo()
 	(&book).printStrInfo()
+
+	rect1 := new(Rect)
+
+	rect2 := &Rect{}
+
+	rect3 := &Rect{0, 0, 100, 200}
+
+	rect4 := &Rect{width: 100, height: 200}
+
+	rect5 := Rect{1, 2, 3, 4}
+
+	fmt.Println(rect1, rect2, rect3, rect4, rect5)
+	fmt.Printf("%v\n%T\n", rect1, rect1)
+	fmt.Printf("%v\n%T\n", rect5, rect5)
 }
 
 type Book struct {
@@ -25,4 +40,9 @@ func (book Book) printInfo() {
 
 func (book *Book) printStrInfo() {
 	fmt.Println(book.title + ", " + strconv.Itoa(book.prize))
+}
+
+type Rect struct {
+	x, y          float64
+	width, height float64
 }
